baa2414832292a9de729aee1a84c1e6ae011aaf8
raefaldhia <raefaldhiamartya@gmail.com>
2017-01-03 12:46:47 +0700

    Implement transformations
    Implementing transformations for panning and zooming.

--- src/MapsView.cpp
+++ src/MapsView.cpp
@@ -15,6 +15,7 @@ MapsView::MapsView()
 	bitmap		= NULL;				
 
 	virtualScroller = new VirtualScroller(this);
+	IsTransformNext = false;
 
 	MapsData::AddHandler(this);	
 	MapsData::Retrieve();
@@ -51,6 +52,15 @@ void MapsView::MouseDown(BPoint where) {
 void MapsView::MouseUp(BPoint where) {
 	if (IsMouseDown) {
 		IsMouseDown = false;
+		if (IsTransformNext) {
+			translatePoint.x += (where.x - pastPoint.x);
+			translatePoint.y += (where.y - pastPoint.y);
+		}
+		else {
+			translatePoint.x = (where.x - pastPoint.x);
+			translatePoint.y = (where.y - pastPoint.y);
+		}
+		IsTransformNext = true;
 
 		MapsVector mapsVector = MapsData::GetVector();
 		if (pastPoint.x < where.x) {
@@ -69,14 +79,48 @@ void MapsView::MouseUp(BPoint where) {
 	}
 }
 
+void MapsView::MouseMoved(BPoint where, uint32 code, const BMessage* dragMessage) {
+	if (IsMouseDown) {
+		MapsVector mapsVector = MapsData::GetVector();
+
+		BAffineTransform transform;
+		if (IsTransformNext) {
+			transform.PreTranslateBy(translatePoint.x, translatePoint.y);
+		}
+		transform.TranslateBy((where.x - pastPoint.x), (where.y - pastPoint.y));
+		SetTransform(transform);
+	}
+}
+
 void MapsView::MessageReceived(BMessage* message) {
 	switch (message->what) {
 		case MAPDATA_UPDATE: {
 			bitmap = BTranslationUtils::GetBitmap(MapsData::Get());
+			IsTransformNext = false;
+			translatePoint.x = 0;
+			translatePoint.y = 0;
+
+			BAffineTransform transform;
+			SetTransform(transform);
 			Invalidate();
 		}break;
 		case VIRTUAL_SCROLLER: {
+			MapsVector mapsVector0 = MapsData::GetVector();
 			MapsData::SetZoom(20 - (message->GetFloat("value", 0.0) / 3));
+			MapsVector mapsVector1 = MapsData::GetVector();
+
+			BPoint center;
+			center.x = Bounds().right / 2;
+			center.y = Bounds().bottom / 2;
+
+			BAffineTransform transform;
+			if (mapsVector1.zoom > mapsVector0.zoom) {
+				transform.ScaleBy(center, pow(2.0, mapsVector1.zoom));
+			}
+			else if (mapsVector1.zoom != 0) {
+				transform.ScaleBy(center, sqrt(mapsVector1.zoom));
+			}
+			SetTransform(transform);
 			MapsData::Retrieve();
 		}break;
 		default: {
